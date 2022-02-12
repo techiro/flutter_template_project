35dd0ba9ff6836eef9ce1e64b86ddbb6df002cc2
techiro <techiromen0@gmail.com>
2022-02-13 02:28:20 +0900

    :zap: 画面遷移のコード

--- packages/flutter_template/lib/widgets/home/home_page.dart
+++ packages/flutter_template/lib/widgets/home/home_page.dart
@@ -1,6 +1,8 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_template/gen/assets.gen.dart';
 import 'package:flutter_template/l10n/l10n.dart';
+import 'package:flutter_template/routes/main_router.gr.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -17,6 +19,12 @@ class HomePage extends StatelessWidget {
           children: <Widget>[
             flutterGenSample(),
             l10nTextWidget(L10n.of(context)!),
+            TextButton(
+              child: Text('sound'),
+              onPressed: () {
+                AutoRouter.of(context).push(const SecondRoute());
+              },
+            )
           ],
         ),
       ),
