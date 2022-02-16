b929e07d2d7a5c2550dc6aad0c50b48783dff6e1
techiro <techiromen0@gmail.com>
2022-02-16 13:17:42 +0900

    :art: iosFlavor設定

--- packages/flutter_template/ios/scripts/retrieve_dart_defines.sh
+++ packages/flutter_template/ios/scripts/retrieve_dart_defines.sh
@@ -5,10 +5,11 @@ done | (
   read -r flavor
   if [ "$flavor" = "" ]; then
     # SRCROOT = ./flutter_template/ios
-    echo "#include \"develop.xcconfig\"\n//FLAVORは反映されていない" >$SRCROOT/Flutter/DartDefine.xcconfig
+    echo "#include \"dev.xcconfig\"\n//FLAVORは反映されていない" >$SRCROOT/Flutter/DartDefine.xcconfig
+    echo "" | tee $SRCROOT/log.txt # デバック用
   else
     echo "#include \"$flavor.xcconfig\"" >$SRCROOT/Flutter/DartDefine.xcconfig
-    # echo $SRCROOT | tee $SRCROOT/log.txt # デバック用
-    \cp -f $SRCROOT/$flavor/GoogleService-Info.plist $SRCROOT/Runner/GoogleService-Info.plist
+    echo $flavor | tee $SRCROOT/log.txt # デバック用
+    # \cp -f $SRCROOT/$flavor/GoogleService-Info.plist $SRCROOT/Runner/GoogleService-Info.plist
   fi
 )
