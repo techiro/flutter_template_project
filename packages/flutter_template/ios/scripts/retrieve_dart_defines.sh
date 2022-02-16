89e2836fec5c3a726ae86a2adcb679a7c86c01f9
techiro <techiromen0@gmail.com>
2022-02-16 12:03:30 +0900

    :building_construction: androidのFlavor設定

---
+++ packages/flutter_template/ios/scripts/retrieve_dart_defines.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+echo $DART_DEFINES | tr ',' '\n' | while read -r line; do
+  echo $line | base64 -d | tr ',' '\n' | xargs -I@ bash -c "echo @ | grep 'FLAVOR' | sed 's/.*=//'"
+done | (
+  read -r flavor
+  if [ "$flavor" = "" ]; then
+    # SRCROOT = ./flutter_template/ios
+    echo "#include \"develop.xcconfig\"\n//FLAVORは反映されていない" >$SRCROOT/Flutter/DartDefine.xcconfig
+  else
+    echo "#include \"$flavor.xcconfig\"" >$SRCROOT/Flutter/DartDefine.xcconfig
+    # echo $SRCROOT | tee $SRCROOT/log.txt # デバック用
+    \cp -f $SRCROOT/$flavor/GoogleService-Info.plist $SRCROOT/Runner/GoogleService-Info.plist
+  fi
+)
